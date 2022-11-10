b18b47e5512166d6224442cbc3d486fcd9132c38
Chaeryung <chaeryun00@gmail.com>
2022-11-10 16:54:30 +0900

    Day46

---
+++ 11주차/1109/017/모듈화_1/Main.js
@@ -0,0 +1,5 @@
+export default function (){
+    console.log('나 실행되었어!')
+}
+// 밖으로 빼줄건데 얘를 기준으로 빼줘라
+// <script type="module" src="index.js">로 해줘야 콘솔 확인 가능
---
+++ 11주차/1109/017/모듈화_1/index.js
@@ -0,0 +1,5 @@
+import Main from './Main.js'
+
+// liveserver로 작동해야 함
+// Main.js에서 Main 이라는 애를 가져와서 실행시킴
+Main()
